3543a467738d119847a2a8a19719fdb1fb8f8d71
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2021-07-13 03:36:56 +0400

    unfinished:Weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import sunny from "./images/01.svg";
+import cloudy from "./images/09.svg";
+import rainy from "./images/17.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "";
 
-export default App;
+const weatherInfo = [{
+        weekDey: "Monday",
+        imgURL: "sunny",
+        temp: "36C"
+    },
+    {
+        weekDey: "Tuesday",
+        imgURL: "cloudy",
+        temp: "32C",
+    },
+    {
+        weekDey: "Wednesday",
+        imgURL: "rainy",
+        temp: "35C"
+
+    },
+    {
+        weekDey: "Thursday",
+        imgURL: "rainy",
+        temp: "35C"
+    },
+    {
+        weekDey: "Friday",
+        imgURL: "rainy",
+        temp: "36C"
+    },
+    {
+        weekDey: "Saturday",
+        imgURL: "sunny",
+        temp: "37C"
+    },
+    {
+        weekDey: "Sunday",
+        imgURL: "cloudy",
+        temp: "39C"
+    }
+];

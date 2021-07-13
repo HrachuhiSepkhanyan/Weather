888803d7737456933579c6f48cae20a359707b5c
Hrachuhi Sepkhanyan <hrachuhisepkhanyan@gmail.com>
2021-07-13 17:05:50 +0400

    finished:weather

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import sunny from "./images/01.svg";
-import cloudy from "./images/09.svg";
-import rainy from "./images/17.svg";
+import weatherInfo from "./components/weatherInfo";
+import "./App.css";
 
-import "";
+console.log(weatherInfo);
+function App() {
+    return (
+       <div className="forecast">{weatherInfo}</div> 
+    )};
 
-const weatherInfo = [{
-        weekDey: "Monday",
-        imgURL: "sunny",
-        temp: "36C"
-    },
-    {
-        weekDey: "Tuesday",
-        imgURL: "cloudy",
-        temp: "32C",
-    },
-    {
-        weekDey: "Wednesday",
-        imgURL: "rainy",
-        temp: "35C"
-
-    },
-    {
-        weekDey: "Thursday",
-        imgURL: "rainy",
-        temp: "35C"
-    },
-    {
-        weekDey: "Friday",
-        imgURL: "rainy",
-        temp: "36C"
-    },
-    {
-        weekDey: "Saturday",
-        imgURL: "sunny",
-        temp: "37C"
-    },
-    {
-        weekDey: "Sunday",
-        imgURL: "cloudy",
-        temp: "39C"
-    }
-];
+export default App;
